83731f054ab83cf6c5e1e89d896547c9f9ebb182
Joris Vankerschaver <jvankerschaver@enthought.com>
2015-11-08 21:32:35 +0000

    ENH: Forward-backward algorithm for discrete observations.

--- simple_hsmm/hsmm.py
+++ simple_hsmm/hsmm.py
@@ -1,10 +1,14 @@
 import numpy as np
 
-from ._base import _viterbi_impl
+from ._base import _viterbi_impl, _fb_impl
 from .properties import (
     AbstractEmissions, ContinuousEmissions, DiscreteEmissions,
     Duration, TransitionMatrix
 )
+
+
+class NoConvergenceError(Exception):
+    pass
 
 
 class BaseHSMMModel(object):
@@ -121,6 +125,108 @@ class MultinomialHSMMModel(BaseHSMMModel):
     def _compute_likelihood(self, obs):
         obs = np.squeeze(obs)
         return np.vstack([rv.pmf(obs) for rv in self._emission_rvs])
+
+    def fit(self, obs, max_iter=20, atol=1e-5, censoring=True):
+        """Fit HSMM parameters (in place), given a sequence of observations.
+
+        """
+
+        censoring = int(censoring)
+        tau = len(obs)
+        j = self.n_states
+        m = self.n_durations
+
+        f = np.zeros((j, tau))
+        l = np.zeros((j, tau))
+        g = np.zeros((j, tau))
+        l1 = np.zeros((j, tau))
+        n = np.zeros(tau)
+        norm = np.zeros((j, tau))
+        eta = np.zeros((j, m))
+        xi = np.zeros((j, m))
+
+        log_likelihoods = []
+
+        llh = -np.inf
+        has_converged = False
+
+        # Make a copy of HSMM elements, so that we can recover in case of
+        # error.
+        old_tmat = self.tmat.copy()
+        old_durations = self.durations.copy()
+        old_emissions = self.emissions.copy()
+        old_startprob = self._startprob.copy()
+
+        # TODO Make startprob into a property like the rest.
+
+        for step in range(1, max_iter + 1):
+            durations_flat = self._durations_flat.copy()
+            tmat_flat = self._tmat_flat.copy()
+            startprob = self._startprob.copy()
+            likelihoods = self._compute_likelihood(obs)
+
+            likelihoods[likelihoods < 1e-12] = 1e-12
+
+            err = _fb_impl(
+                censoring, tau, j, m,
+                durations_flat, tmat_flat, startprob,
+                likelihoods.reshape(-1),
+                f.reshape(-1), l.reshape(-1), g.reshape(-1), l1.reshape(-1),
+                n, norm.reshape(-1), eta.reshape(-1), xi.reshape(-1)
+            )
+            print err
+            if err != 0:
+                break
+
+            # Calculate log likelihood
+            new_llh = np.log(n).sum()
+            log_likelihoods.append(new_llh)
+            if np.abs(new_llh - llh) < atol:
+                has_converged = True
+                break
+            llh = new_llh
+
+            # Re-estimate pi
+            new_pi = l[:, 0]
+
+            # Re-estimate tmat
+            new_tmat = np.empty_like(self._tmat)
+            for i in range(j):
+                r = l1[i, :-1].sum()
+                for k in range(j):
+                    z = self._tmat[i, k] * (g[k, 1:] * f[i, :-1]).sum()
+                    new_tmat[i, k] = z / r
+
+            # Re-estimate durations
+            denominator = l1[:, :-1].sum(axis=1)
+            if censoring:
+                denominator += l[:, -1]
+            new_durations = eta / denominator[:, np.newaxis]
+
+            # Re-estimate discrete emissions
+            new_emissions = np.empty_like(self._emissions)
+            for em in range(self._emissions.shape[1]):
+                mask = obs == em
+                new_emissions[:, em] = l[:, mask].sum(axis=1) / l.sum(axis=1)
+
+            # Reassign!
+            self.tmat = new_tmat
+            self.durations = new_durations
+            self.emissions = new_emissions
+            self._startprob = new_pi
+
+        if err != 0:
+            # An error occurred.
+            self.tmat = old_tmat
+            self.emissions = old_emissions
+            self.durations = old_durations
+            self._startprob = old_startprob
+            raise NoConvergenceError(
+                "The forward-backward algorithm encountered an internal error "
+                "after {} steps. Try reducing the `num_iter` parameter. "
+                "Log-likelihood procession: {}.".format(step, log_likelihoods))
+
+        return has_converged, llh
 
 
 class ContinuousHSMMModel(BaseHSMMModel):
