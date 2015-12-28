55a7dfa93d5160bac2f3242e85654f72df5aba54
Joris Vankerschaver <jvankerschaver@enthought.com>
2015-12-28 19:00:21 +0000

    MAINT: Use pre-built wheels

--- appveyor-install.cmd
+++ appveyor-install.cmd
@@ -1,24 +1,13 @@
 "%sdkverpath%" -q -version:"%sdkver%"
 call setenv /x64
 
-rem install openblas for scipy
-
-mkdir Downloads
-cd Downloads
-appveyor DownloadFile http://downloads.sourceforge.net/project/openblas/v0.2.15/OpenBLAS-v0.2.15-Win64-int32.zip
-7z e OpenBLAS-v0.2.15-Win64-int32.zip
-dir
-copy libopenblas.dll C:\
-cd %APPVEYOR_BUILD_FOLDER%
-dir C:\
-
 rem install python packages
 pip install --cache-dir C:/egg_cache nose
 pip install --cache-dir C:/egg_cache coverage==3.7.1
-pip install --cache-dir C:/egg_cache numpy
-pip install --cache-dir C:/egg_cache scipy
 pip install --cache-dir C:/egg_cache cython
 pip install --cache-dir C:/egg_cache six
+pip install -i https://pypi.binstar.org/carlkl/simple --cache-dir C:/egg_cache numpy
+pip install -i https://pypi.binstar.org/carlkl/simple --cache-dir C:/egg_cache scipy
 
 rem install package
 python setup.py develop
